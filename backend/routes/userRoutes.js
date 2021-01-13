bf15d95d9256a83e22786a1e9f31d7c7815e9597
Victor Vermeulen <vcvermeulen@gmail.com>
2021-01-13 16:08:00 +0100

    password encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 //these 2 do the same thing
@@ -8,5 +12,7 @@ import { protect } from "../middleware/authMiddleware.js";
 router.route("/login").post(authUser);
 
 router.route("/profile").get(protect, getUserProfile);
+
+router.route("/").post(registerUser);
 
 export default router;
